022143883f0f83ab95918838c804fee1d4cbd469
David <david.samuelsson@ni.com>
2023-11-01 11:43:53 +0100

    Update

--- Aliases/Aliases.py
+++ Aliases/Aliases.py
@@ -21,12 +21,27 @@ from NationalInstruments.VeriStand import Error
 
 current_dir = os.path.dirname(__file__)
 # See http://pythonnet.github.io/ for PyhtonNet documentation
+# Configure the logging system
 
+log_filename = "my_log_file.log"
+
+# Set the logging level to control which messages are recorded in the log file
+logging.basicConfig(
+    level=logging.INFO,  # Set the minimum level to INFO
+    filename=log_filename,  # Specify the log file name
+    filemode="a",  # Append to the log file
+    format="%(asctime)s [%(levelname)s]: %(message)s",  # Log message format
+)
 
 class AliasController:
     def __init__(self):
         self._system_definition_object = None
         self._first_target = None
+        self.error = Error()
+        self.error_out = Error()
+        self.success =  System.Boolean(False)
+        #For Custom device references
+        self.nodeIDUtil = NodeIDUtil()
 
     def create_system_definition_src(self, ip_address_in = '192.168.0.3', data_rate_in = 1000):
         """
@@ -51,7 +66,7 @@ class AliasController:
             self._first_target.IPAddress = ip_address_in # ip_address
             self._first_target.TargetRate = data_rate_in # data_rate
 
-            self.SaveSystemDefinition()
+            self.save_system_definition()
         except Exception as e:
             logging.error(f"Error creating system definition: {e}")
     
@@ -70,83 +85,90 @@ class AliasController:
             # Set properties
             self._first_target.IPAddress = ip_address # ip_address
             self._first_target.TargetRate = data_rate # data_rate
-
-            self.SaveSystemDefinition()
+            self._first_target.OperatingSystem = target_type
+            self.save_system_definition()
         except Exception as e:
             logging.error(f"Error creating system definition: {e}")
 
-    def SaveSystemDefinition(self):
-        # Save SystemDefinitionFile
+    def save_system_definition(self):
+        """
+        Save the system definition file
+        """
         error = System.String("")
         error_out = System.String("")
         functionReturn, error_out = self._system_definition_object.SaveSystemDefinitionFile(error)
 
         if (not error_out):
-            print("File saved")
+            log = "File saved"
         else:
-            print(error_out)
+            log = error_out
+        logging.info(log)
 
     def add_aliases_folder(self):
-        error = Error()
-        error_out = Error()
-        success =  System.Boolean(False)
-
+        """
+        Add aliases folder using GetCustomDevices 
+        """
         ECU = self._first_target.GetCustomDevices().GetCustomDeviceList()[0].GetChildren()
-        [print("ECU: ", i.Name,i.Description ) for i in ECU]
         frames = self._first_target.GetCustomDevices().GetCustomDeviceList()[0].GetChildren()[1].GetChildren()
-        [print("frames: ", i.Name, ) for i in frames]
         CAN_messages = self._first_target.GetCustomDevices().GetCustomDeviceList()[0].GetChildren()[1].GetChildren()[0].GetChildren()
-        [print("CAN_messages: ", i.Name, i.NodePath ) for i in CAN_messages]
+
 
         CD_folder_name_1 = System.String(ECU[1].Name) 
         CD_folder_name_2 = System.String(ECU[2].Name)
         CD_folder_desc_1 = System.String(ECU[1].Description)
         CD_folder_desc_2 = System.String(ECU[2].Description)
         #print("sgda",not str(CD_folder_desc_1).strip()) #string empty or only whitespaces
-        print("DESC:{}".format(CD_folder_name_1), "DESC:{}".format(CD_folder_name_2))
-        print("DESC:{}".format(CD_folder_desc_1), "DESC:{}".format(CD_folder_desc_2))
 
         aliases = self._system_definition_object.Root.GetAliases().GetAliasesList()
-        [print("Aliases: ", i.Name, i.NodePath) for i in aliases]
+
         alias_folder_1 = AliasFolder(CD_folder_name_1,"test")
         alias_folder_2 = AliasFolder(CD_folder_name_2,"test")
-        success, error_out = self._system_definition_object.Root.GetAliases().AddAliasFolder(alias_folder_1,error)
-        print(success,error)
-        success, error_out = self._system_definition_object.Root.GetAliases().AddAliasFolder(alias_folder_2,error)
+        success, self.error_out = self._system_definition_object.Root.GetAliases().AddAliasFolder(alias_folder_1,self.error)
+        success, self.error_out = self._system_definition_object.Root.GetAliases().AddAliasFolder(alias_folder_2,self.error)
+        logging.info("Alias folders created successfully")
 
     def add_aliases(self):
-        error = Error()
-        error_out = Error()
-        success =  System.Boolean(False)
-        nodeIDUtil = NodeIDUtil()
+        """
+        Add aliases using custom device references IDToCustomDeviceSection
+        """
+        logging.info("Adding aliases")
         custom_devices = self._first_target.GetCustomDevices().GetCustomDeviceList()
         custom_device_vcom = [i.NodeID for i in custom_devices if i.Name == "VCOM"]
         custom_device_vcom_id = custom_device_vcom[0]
-
 
         class ECU(Enum):
             Status = 0
             ADAS = 1
             DAQ = 2
 
-        #ta length av ECU,CAN_message och signals, skapa matrix, dubbel loop ECU, CAN_message -> array of lists with signals
-        for ECU_index, ECU_value in enumerate(nodeIDUtil.IDToCustomDeviceSection(custom_device_vcom_id).GetChildren()):
-            # print("ECU insdex", ECU_index)
+        for ECU_index, ECU_value in enumerate(self.nodeIDUtil.IDToCustomDeviceSection(custom_device_vcom_id).GetChildren()):
             if ECU_index > 0:
-                for frames_index, frames_value in enumerate(nodeIDUtil.IDToCustomDeviceSection(custom_device_vcom_id).GetChildren()[ECU_index].GetChildren()):
-                    # print("ECU insdex", ECU_index,"framendex", frames_index, "framval", frames_value.Name)
-                    for CAN_message_index, CAN_message_value in enumerate(nodeIDUtil.IDToCustomDeviceSection(custom_device_vcom_id).GetChildren()[ECU_index].GetChildren()[frames_index].GetChildren()):
-                        # print("Messgae index",CAN_message_value.Name)
-                        for signals in nodeIDUtil.IDToCustomDeviceSection(custom_device_vcom_id).GetChildren()[ECU_index].GetChildren()[frames_index].GetChildren()[CAN_message_index].GetChildren():
-                            print("ECU: ", ECU_value.Name,"Frame: ",frames_index,"CAN_Message: ",CAN_message_value.Name,"Signalname: ", signals.Name)
-                            write_alias = Alias(signals.Name,signals.NodePath,signals.NodePath)
+                for frames_index, frames_value in enumerate(ECU_value.GetChildren()):
+                    for CAN_message_index, CAN_message_value in enumerate(frames_value.GetChildren()):
+                        for signals in CAN_message_value.GetChildren():
+                            ECU_name = ECU_value.Name
+                            frame_name = frames_value.Name
+                            CAN_message_name = CAN_message_value.Name
+                            signal_name = signals.Name
+
+                            logging.info("ECU: {}, Frame: {}, CAN_Message: {}, SignalName: {}".format(ECU_name, frame_name, CAN_message_name, signal_name))
+
+                            write_alias = Alias(signal_name, signals.NodePath, signals.NodePath)
+
                             if ECU_index == ECU.ADAS.value:
-                                success, error_out = self._system_definition_object.Root.GetAliases().GetAliasFolderList()[ECU_index-1].AddAlias(write_alias,error) # add to folder
-                                print("ADAS aliases printed",success,error_out)
-                            if ECU_index == ECU.DAQ.value:
-                                success, error_out = self._system_definition_object.Root.GetAliases().GetAliasFolderList()[ECU_index-1].AddAlias(write_alias,error)
-                                print("DAQ aliases printed",success,error_out)
-        self.SaveSystemDefinition()
+                                # folder_index = ECU_index - 1
+                                folder_index = 3 # Hardcoded for Close loop Control Example - ADAS.nivssdf_copy
+                                success, error_out = self._system_definition_object.Root.GetAliases().GetAliasFolderList()[folder_index].AddAlias(write_alias, self.error)
+                                log = "ADAS aliases printed" 
+                                logging.info("ADAS aliases printed {} {}".format(success, error_out))
+                            elif ECU_index == ECU.DAQ.value:
+                                # folder_index = ECU_index - 1
+                                folder_index = 4 # Hardcoded for Close loop Control Example - ADAS.nivssdf_copy
+                                success, error_out = self._system_definition_object.Root.GetAliases().GetAliasFolderList()[folder_index].AddAlias(write_alias, self.error)
+                                log = "DAQ aliases printed"
+                            logging.info("{} {} {}".format(log, success, error_out))
+
+        self.save_system_definition()
 
 
 if __name__ == "__main__":
@@ -161,13 +183,11 @@ if __name__ == "__main__":
                         help='Path+name of file to append to')
     args = parser.parse_args()
 
-    dst = r"C:\Users\User\Documents\VeriStand Projects\Test\Test_copy.nivssdf"
+    # dst = r"C:\Users\User\Documents\VeriStand Projects\Test\Test_copy.nivssdf"
+    dst = r"C:\Users\User\Documents\VeriStand Projects\Veristand 2023 Q4\Veristand 2023 Q4\Close loop Control Example - ADAS\Close loop Control Example - ADAS.nivssdf_copy"
     shutil.copyfile(args.SysDef, dst)
     
     aliasObject.create_system_definition(args.SysDef)
     aliasObject.add_aliases_folder()
     aliasObject.add_aliases()
 
-    # print("Copied file: {}".format(dst), "\n")
-    # print("inputted file: {}".format(args.SysDef), "\n")
-    # C:\Users\User\Documents\VeriStand Projects\Test
