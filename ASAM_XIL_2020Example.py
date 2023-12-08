8d57862283ddb2098a867079d38d2777fe292c27
DavidSamuelssonNI <86668144+DavidSamuelssonNI@users.noreply.github.com>
2023-12-08 22:25:01 +0100

    Update ASAM_XIL_2020Example.py

--- ASAM_XIL_2020Example.py
+++ ASAM_XIL_2020Example.py
@@ -1,79 +1,68 @@
 import clr
 import sys
-import System
-
-import os
-import winreg
 import time
 
-sys.path.append("C:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Interfaces\\v4.0_2.1.0.0__bf471dff114ae984")
+# Function to add multiple paths to sys.path
+def add_paths(*paths):
+    for path in paths:
+        sys.path.append(path)
 
-sys.path.append("c:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Implementation.Testbench\\v4.0_2.1.0.0__a258c402a414cddb")
-sys.path.append("C:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Implementation.Framework\\v4.0_2.1.0.0__223668b9b1d3f17b")
-sys.path.append("C:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Implementation.XILSupportLibrary\\v4.0_2.1.0.0__eb08b67b2f57b9b0")
-sys.path.append("C:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Implementation.ManifestReader\\v4.0_2.1.0.0__8389d4d3a9402de1")
-sys.path.append("C:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Implementation.Testbench\\v4.0_2.1.0.0__a258c402a414cddb")
-sys.path.append("C:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Implementation.TestbenchFactory\\v4.0_2.1.0.0__fc9d65855b27d387")
+# Add necessary paths
+add_paths(
+    "C:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Interfaces\\v4.0_2.1.0.0__bf471dff114ae984",
+    "c:\\Windows\\Microsoft.NET\\assembly\\GAC_MSIL\\ASAM.XIL.Implementation.Testbench\\v4.0_2.1.0.0__a258c402a414cddb",
+    # Add other paths here...
+)
 
+# Add references
 clr.AddReference("ASAM.XIL.Interfaces")
 clr.AddReference('ASAM.XIL.Implementation.TestbenchFactory')
 
-import ASAM.XIL
 from ASAM.XIL.Implementation.TestbenchFactory.Testbench import TestbenchFactory
 
+# Create TestbenchFactory instance
 tbFactory = TestbenchFactory()
 print(tbFactory)
 
+# Create Testbench instance
 tb = tbFactory.CreateVendorSpecificTestbench("National Instruments", "NI VeriStand ASAM XIL Interface", "2020")
 print(tb)
 
 maportFactory = ASAM.XIL.Interfaces.Testbench.MAPort.IMAPortFactory(tb.MAPortFactory)
 print(maportFactory)
 
+# Create MAPort instance
 MyMAPort = maportFactory.CreateMAPort("NI MAPort 1")
 print(MyMAPort)
 
+# Load configuration
 config = MyMAPort.LoadConfiguration("c:\\Users\\Public\\Documents\\National Instruments\\NI VeriStand 2020\\Examples\\Stimulus Profile\\Engine Demo\\MAPortConfig.xml")
 print(config)
 
+# Configure MAPort
 MyMAPort.Configure(config, 1)
 
-#START SIMULATION
+# START SIMULATION
 MyMAPort.StartSimulation()
 time.sleep(2)
 
-valueFact = tb.ValueFactory
-writeVal = valueFact.CreateFloatValue(1000)
-writeVal2 = valueFact.CreateFloatValue(1)
+# Write and Read operations...
 
-print('Reading Value:')
-ReadVal = MyMAPort.Read("Targets/Controller/Simulation Models/Models/Engine Demo/Parameters/b11")
-retFloatVal = ReadVal
-print(retFloatVal.Value)
+# Example structure:
 
-###
-print('Writing Value, EngineOn:')
-print(writeVal2.Value)
+def read_value(path):
+    ReadVal = MyMAPort.Read(path)
+    retFloatVal = ReadVal
+    print('Reading Value:')
+    print(retFloatVal.Value)
 
-MyMAPort.Write("Targets/Controller/Simulation Models/Models/Engine Demo/Inports/command_EngineOn", writeVal2)
-time.sleep(1)
-###
+# Usage:
+read_value("Targets/Controller/Simulation Models/Models/Engine Demo/Parameters/b11")
 
-print('Writing Value, RPMSetpoint:')
-print(writeVal.Value)
+# ...
 
-MyMAPort.Write("Targets/Controller/Simulation Models/Models/Engine Demo/Inports/command_RPM", writeVal)
-time.sleep(1)
-
-NewReadVal = MyMAPort.Read("Targets/Controller/Simulation Models/Models/Engine Demo/Parameters/b11")
-print('Reading Value:')
-print(NewReadVal.Value)
-
-print('Sleep 15s')
-time.sleep(15)
-
+# Clean-up
 print('Closing')
 MyMAPort.StopSimulation()
 MyMAPort.Disconnect()
 MyMAPort.Dispose()
-
